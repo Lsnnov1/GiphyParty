9c0c530d079de190c6a1cf6561ad8a8d6b9eacdf
Lsnnov1 <154484671+Lsnnov1@users.noreply.github.com>
2024-05-02 14:30:17 -0400

    annotations

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 console.log("Let's get this party started!");
+
+// select values using jquery
 const $searchbar = $("#search")
 const $gifDiv = $("#gifDiv")
 
+// create async function that adds gif
 async function getGif(res){
+    // get response data length
     let numResults = res.data.length;
+    // if response data not empty:
     if (numResults) {
+    // select random gif out of response.data
     let randomIdx = Math.floor(Math.random() * numResults);
     let $newCol = $("<div>", { class: "gifColumn" });
     let $newGif = $("<img>", {
@@ -17,24 +23,29 @@ async function getGif(res){
 
 }
 
-
+// jquery eventlistener on form
 $("form").on("submit", async function(e) {
     e.preventDefault();
     console.log("form submitted")
+
+    // select searchbar value
     let searchQ = $searchbar.val();
+    // then reset
     $searchbar.val("");
     console.log("SEARCHQ", searchQ)
 
+    // use axios to login and get retrive response
     const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        // set params
         params: {
           q: searchQ,
           api_key: "E8oGIx9SX977sXFVbL7V4Cfy2vLSbmVz"
         }});
-
+    // call function on response data
     getGif(res.data)
     console.log("function called")
 });
-
+// eventlistener to button when clicked, empty content div
 $("#btn-remove").on("click", function(){
     $gifDiv.empty()
 })
